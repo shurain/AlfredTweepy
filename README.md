cfdeb518a320c18e1fd2448ed599c0c7620a6157
Sungjoo Ha <shurain@gmail.com>
2012-08-08 22:05:14 +0900

    Working version

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 AlfredTweepy
 ============
 
-This is an awesome Alfred extension for quick tweets.
+Alfred extension for quick tweets.
+Handles unicode string perfectly.
+
+Requirements
+------------
+
+* [Tweepy](http://tweepy.github.com/)
+* [Twitter API Key](https://dev.twitter.com/) - Used to allow tweepy to access your account
+* Growl
+
+Usage
+-----
+Modify `twitter_auth.py` file and set the Twttier API keys appropriately.
+Simple `tw {query}` should work within Alfred.
---
+++ alfredtweepy.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python
+import sys
+
+import twitter_auth as ta
+
+def tweetq(q):
+    q = q.decode('utf-8')
+    tweets = []
+
+    #TODO handle a long string with only non-whitespace characters
+    tweet = u''
+    for word in q.split():
+        tmp = tweet
+        tmp += u' ' + word
+        if len(tmp) > 136:
+            tweet += ' [' + str(len(tweets)) + ']'
+            tweets.append(tweet)
+            tweet = u''
+        else:
+            tweet = tmp
+    tweets.append(tweet)
+
+    for t in tweets:
+        try:
+            ta.api.update_status(t.encode('utf-8'))
+            print 'Tweeted:', t.encode('utf-8')
+        except:
+            print 'Unabled to tweet:', sys.exc_info()[0]
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit(2) # no argument provided
+
+    query = sys.argv[1] # assume proper escapes
+    tweetq(query)
